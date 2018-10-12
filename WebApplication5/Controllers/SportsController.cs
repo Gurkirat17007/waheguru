e9430a3a7e647b6a8e0dc277e4de3e19821dd505
Gurkirat17007 <41137077+Gurkirat17007@users.noreply.github.com>
2018-10-12 15:59:55 -0400

    Initial commit

---
+++ WebApplication5/Controllers/SportsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class SportsController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: Sports
+        public ActionResult Index()
+        {
+            var sports = db.Sports.Include(s => s.Team);
+            return View(sports.ToList());
+        }
+
+        // GET: Sports/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sport sport = db.Sports.Find(id);
+            if (sport == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sport);
+        }
+
+        // GET: Sports/Create
+        public ActionResult Create()
+        {
+            ViewBag.Boys = new SelectList(db.Teams, "Boys", "Girls");
+            return View();
+        }
+
+        // POST: Sports/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Boys,Soccer,Baseball,Rugby")] Sport sport)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sports.Add(sport);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Boys = new SelectList(db.Teams, "Boys", "Girls", sport.Boys);
+            return View(sport);
+        }
+
+        // GET: Sports/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sport sport = db.Sports.Find(id);
+            if (sport == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Boys = new SelectList(db.Teams, "Boys", "Girls", sport.Boys);
+            return View(sport);
+        }
+
+        // POST: Sports/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Boys,Soccer,Baseball,Rugby")] Sport sport)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sport).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Boys = new SelectList(db.Teams, "Boys", "Girls", sport.Boys);
+            return View(sport);
+        }
+
+        // GET: Sports/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sport sport = db.Sports.Find(id);
+            if (sport == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sport);
+        }
+
+        // POST: Sports/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sport sport = db.Sports.Find(id);
+            db.Sports.Remove(sport);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
